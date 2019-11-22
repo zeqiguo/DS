7fe87b10b571a9f5aca32e93f1be15c9c8404524
zeqiguo <zeqig@smu.edu>
2019-11-22 16:57:37 -0600

    finish unorder list

--- linked_list/unordered_list.py
+++ linked_list/unordered_list.py
@@ -48,14 +48,94 @@ class UnorderList:
         return found
     
     def remove_(self, item):
-        pass
+        curr = self.head
+        prev = None
+        found = False
+        while not found:
+            if curr.get_data() == item:
+                found = True
+            else:
+                prev = curr
+                curr = curr.get_next()
+
+        if prev == None:
+            self.head = curr.get_next()
+        else:
+            prev.set_next((curr.get_next()))
+
+    def append_(self, item):
+        curr = self.head
+        prev = None
+        new_node = Node(item)
+        while curr:
+            prev = curr
+            curr = curr.get_next()
+
+        prev.set_next(new_node)
+
+    def index_(self, item):
+        curr = self.head
+        index = 0
+        while curr:
+            if curr.get_data() == item:
+                return index
+            curr = curr.get_next()
+            index += 1
+
+    def insert(self, index, item):
+        pos = 0
+        prev = None
+        curr = self.head
+        new_node = Node(item)
+        if index == 0:
+            self.add_(item)
+        elif index == self.size_():
+            self.append_(item)
+        else:
+            while curr and index != pos:
+                prev = curr
+                curr = curr.get_next()
+                pos += 1
+            prev.set_next(new_node)
+            new_node.set_next(curr)
+
+    def pop_(self):
+        curr = self.head
+        prev = None
+        while curr.get_next():
+            prev = curr
+            curr = curr.get_next()
+        prev.set_next(None)
+        return curr.get_data()
+
+    def pop_index(self, index):
+        curr = self.head
+        prev = None
+        pos = 0
+        while curr and pos != index:
+            prev = curr
+            curr = curr.get_next()
+            pos += 1
+        prev.set_next(curr.get_next())
+        return curr.get_data()
+
+    def print_(self):
+        curr = self.head
+        while curr:
+            print(curr.get_data())
+            curr = curr.get_next()
+
 
 
 
 ul = UnorderList()
 ul.add_(1)
 ul.add_(2)
-print(ul.search(1))
+ul.append_(0)
+# ul.insert(4, 9)
+print(ul.pop_index(1))
+# ul.print_()
+# print(ul.index_(3))
 # print(ul.size_())
 
 
